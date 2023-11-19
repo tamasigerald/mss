fd00c1e0d1c250efea4c76c4438633f9f1d1dd9f
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2023-11-19 16:38:34 +0100

    run lint + prettier

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,33 +1,31 @@
-import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
-
-import { cn } from '@/lib/utils';
-import { VariantProps, tv } from 'tailwind-variants';
+import * as React from 'react';
+import { type VariantProps, tv } from 'tailwind-variants';
 
 const buttonVariants = tv({
   base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  defaultVariants: {
+    size: 'default',
+    variant: 'default',
+  },
   variants: {
+    size: {
+      default: 'h-9 px-4 py-2',
+      icon: 'h-9 w-9',
+      lg: 'h-10 rounded-md px-8',
+      sm: 'h-8 rounded-md px-3 text-xs',
+    },
     variant: {
       default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
       destructive:
         'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+      ghost: 'hover:bg-accent hover:text-accent-foreground',
+      link: 'text-primary underline-offset-4 hover:underline',
       outline:
         'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
       secondary:
         'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-      ghost: 'hover:bg-accent hover:text-accent-foreground',
-      link: 'text-primary underline-offset-4 hover:underline',
     },
-    size: {
-      default: 'h-9 px-4 py-2',
-      sm: 'h-8 rounded-md px-3 text-xs',
-      lg: 'h-10 rounded-md px-8',
-      icon: 'h-9 w-9',
-    },
-  },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
   },
 });
 
@@ -38,11 +36,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ asChild = false, className, size, variant, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={buttonVariants({ className, size, variant })}
         ref={ref}
         {...props}
       />
